0bf4056553c3db4a6d765b92140b7e4561798e9e
suman <himachalam96550@gmail.com>
2025-06-20 14:50:07 +0530

    Feature_001 commit

--- src/test/java/HelperClass/AlertHelper.java
+++ src/test/java/HelperClass/AlertHelper.java
@@ -1,5 +1,28 @@
 package HelperClass;
 
-public class AlertHelper {
+import org.openqa.selenium.Alert;
+
+import StandardUtils.DriverFactory;
+
+public class AlertHelper extends DriverFactory {
+
+	public void acceptAlert() {
+
+		Alert alert = driver.switchTo().alert();
+		alert.accept();
+	}
+
+	public void dismissAlert() {
+
+		Alert alert = driver.switchTo().alert();
+		alert.dismiss();
+	}
+	
+	public void sendkeysAndAcceptAlert(String text) {
+
+		Alert alert = driver.switchTo().alert();
+		alert.sendKeys(text);
+		alert.accept();
+	}
 
 }
